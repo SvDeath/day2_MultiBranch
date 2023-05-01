533048edb8d083e230e3d0203d1c746f70405981
roman <romanaliev808@gmail.com>
2023-04-30 21:53:31 -0500

    This is juva code for you

---
+++ src/ForNail/NailJava.java
@@ -0,0 +1,16 @@
+package ForNail;
+
+public class NailJava {
+
+    public static void main(String[] args) {
+
+
+        String name = "Nail Mustafa";
+
+        name = name.replace("Mustafa", "Mustakaev");
+
+        System.out.println(name);
+
+
+    }
+}

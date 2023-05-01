70fe749021abd0901a11b1304ed6a11137a68659
nail-92 <nail-92@live.com>
2023-04-30 22:09:10 -0500

    romka2

--- src/papka/Class2.java
+++ src/papka/Class2.java
@@ -2,18 +2,16 @@ package src.papka;
 
 public class Class2 {
     public static void main(String[] args) {
-        double size = 5.0; // Size of the star
-        int starWidth = (int) (size * 2); // Width of the star
+        int size = 5; // Size of the star
+        int halfSize = size / 2; // Half the size of the star
 
         // Loop through rows and columns to draw the star
-        for (int row = 0; row <= starWidth; row++) {
-            for (int col = 0; col <= starWidth; col++) {
-                double distance = Math.sqrt(Math.pow(row - size, 2) + Math.pow(col - size, 2));
-                // If the distance from the center is less than half the size, print '*'
-                if (distance < size / 2) {
-                    System.out.print("*");
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                if (row == halfSize || col == halfSize || row + col == halfSize * 2 || row - col == halfSize) {
+                    System.out.print("*"); // Print '*' for star points
                 } else {
-                    System.out.print(" ");
+                    System.out.print(" "); // Print ' ' for spaces
                 }
             }
             System.out.println(); // Move to the next row

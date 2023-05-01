95132fdc3c28a893fc97c2c689c3abc4df252a1f
nail-92 <nail-92@live.com>
2023-04-30 21:06:45 -0500

    romka

---
+++ src/practice/roma.java
@@ -0,0 +1,9 @@
+package src.practice;
+
+public class roma {
+    public static void main(String[] args) {
+        for (int i = 0; ; i++) {
+            System.out.println("Roma :)");
+        }
+    }
+}
